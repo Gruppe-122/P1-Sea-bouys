6d605d8b95a62d42b27ac0cfcf5df27356a55126
Cwoasount <78689800+eKondrup@users.noreply.github.com>
2025-11-05 16:06:20 +0100

    Feature/currentsensor (#3)
    * added examples folder and ESP32_currentsensor example code
    
    * Refactoring
    
    * Added current source file and header file.
    
    * add tid_m_samples paramter for avg_ADC function
    
    * Small changes in source files
    
    * fix 'integerDivisionByZero' error, moved default params to h file and moved set_sampling to public
    
    * Moved examples to Currentsensor folder
    
    * moved file includes to current.h

---
+++ SeaBouy/src/current.cpp
@@ -0,0 +1,55 @@
+#include "current.h"
+
+CurrentSensor::CurrentSensor(int pin, int dcOffset_mV, int modSensitivity_mV_per_A)
+{
+  _pin = pin;
+  _dcOffset_mV = dcOffset_mV;
+  _mod_mV_per_A = modSensitivity_mV_per_A;
+}
+
+void CurrentSensor::begin()
+{
+  analogReadResolution(_adc_resolution); // Set ADC resolution
+  analogSetAttenuation(ADC_11db);       // Set input attenuation for 0-3.3V range
+  esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+}
+
+void CurrentSensor::set_sampling(int samples, int tid_m_samples, int adc_resolution)
+{
+  _samples = samples;
+  _tid_m_samples = tid_m_samples;
+  _adc_resolution = adc_resolution;
+}
+
+int CurrentSensor::avg_ADC(int samples, int tid_m_samples)
+{
+  long sum = 0;
+  int adc = 0;
+  for (int i = 0; i < samples; i++)
+  {
+    adc = analogRead(_pin);
+    sum += adc;
+    delay(tid_m_samples);
+  }
+  return sum / samples;
+}
+
+float CurrentSensor::get_voltage_mV()
+{
+
+  int avg_adc_val = avg_ADC(_samples, _tid_m_samples);
+  return esp_adc_cal_raw_to_voltage(avg_adc_val, &adc_chars);
+}
+
+float CurrentSensor::measure_current_A()
+{
+  int avg_adc_val = avg_ADC(_samples, _tid_m_samples);
+  uint32_t voltage_mV = get_voltage_mV();
+  float amps = (voltage_mV - _dcOffset_mV) / _mod_mV_per_A;
+  return amps;
+}
+
+float CurrentSensor::measure_current_mA()
+{
+  return 1000 * measure_current_A();
+}
---
+++ SeaBouy/src/current.h
@@ -0,0 +1,89 @@
+#ifndef CURRENT_H
+#define CURRENT_H
+
+#include "current.h"
+#include <Arduino.h>
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+/**
+ * @class CurrentSensor
+ * @brief Reads current from an ACS712 sensor using the ESP32 ADC.
+ *
+ * Example usage:
+ * @code
+ * CurrentSensor currentSensor(19, 2545, 100);
+ * currentSensor.begin();
+ * float current_mA = currentSensor.measure_current_mA();
+ * @endcode
+ */
+
+class CurrentSensor
+{
+public:
+    /**
+     * @brief Constructor for the CurrentSensor class.
+     * @param pin GPIO pin connected to sensor output, make sure PIN has an ADC_Channel.
+     * @param dcOffset_mV Offset voltage in millivolts (around 2550 mV for ACS712).
+     * @param modSensitivity_mV_per_A Sensitivity of sensor in mV per ampere. Is 185 for 5amp version
+     */
+    CurrentSensor(int pin, int dcOffset_mV, int modSensitivity_mV_per_A=185);
+    /**
+     * @brief begin method for CurrentSensor class
+     * It executes the following code
+     * @code
+     * analogReadResolution(ADC_RESOLUTION);
+     * analogSetAttenuation(ADC_11db);
+     * esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+     * @endcode
+     * @note Must be called once before taking any readings.
+     */
+    void begin();
+    /**
+     * @brief sets the sampling for avg_ADC()
+     * @param samples Amount of samples it calculates the average out of
+     * @param tid_m_samples Time between samples
+     * @param adc_resolution ADC resolution in bits 12 for ESP32-S3
+     */
+    void set_sampling(int samples = 20, int tid_m_samples = 20, int adc_resolution = 12);
+    /**
+     * @brief avg_ADC method for CurrentSensor class
+     * Takes the average ADC value over a specified time with a specified interval
+     * @param samples Amount of samples it calculates the average out of
+     * @param tid_m_samples
+     * @code
+     * analogReadResolution(ADC_RESOLUTION);
+     * analogSetAttenuation(ADC_11db);
+     * esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
+     * @endcode
+     * @return average ADC value
+     */
+    int avg_ADC(int samples, int tid_m_samples);
+    /**
+     * @brief get_voltage_mV method for CurrentSensor class
+     * gets the voltage based on the average ADC signal
+     * @return voltage in mV
+     */
+    float get_voltage_mV();
+    /**
+     * @brief Measures the current going through the module
+     * @return current in amps
+     */
+    float measure_current_A();
+    /**
+     * @brief Measures the current going through the module and multiplies with 1000
+     * @return current in milliamps
+     */
+    float measure_current_mA();
+
+private:
+    int _samples;
+    int _tid_m_samples;
+    int _adc_resolution;
+    int _pin;
+    int _dcOffset_mV;
+    float _mod_mV_per_A;
+    esp_adc_cal_characteristics_t adc_chars;
+};
+
+#endif
