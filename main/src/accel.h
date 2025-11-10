c733f1a0e23fdb92dd7b1023e4aba1a282d97bc8
pandekager8 <johannes@vad-mortensen.dk>
2025-11-10 14:52:08 +0100

    accel wakes esp32 up

--- main/src/accel.h
+++ main/src/accel.h
@@ -1,13 +1,9 @@
 #ifndef ACCEL_H
 #define ACCEL_H
 
-#define tyngdeAcc 9.81  //m/s2
-#define maxG 6
-
 #include <Arduino.h>
-#include <Wire.h>  //lader mig kommunikere på I2C
+#include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
 
 int accelSetup();
 int calibrate();
