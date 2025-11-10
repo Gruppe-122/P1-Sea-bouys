d564a7f4c782a60a6c3d10a91c60ce2fe05991e6
marcus <marcus.mr2003@gmail.com>
2025-11-10 12:04:04 +0100

    update var

--- main/src/mesh.cpp
+++ main/src/mesh.cpp
@@ -10,20 +10,22 @@ meshtastic::start_radio()
 {
     // freq, bw, sf, cr, syncWord, power, preambleLen, gain, useTCXO
     int state = radio.begin(
-        868.0, // frequency in MHz
-        125.0, // bandwidth in kHz (7.8–500)
-        7,     // spreading factor (6–12)
-        7,     // coding rate denominator (5–8)
-        0x34,  // sync word (0x34 public, 0x12 private)
-        20,    // power in dBm
-        8,     // preamble length
-        0,     // gain (0 = auto)
-        false  // TCXO enabled
+        frequency,        // frequency in MHz
+        bandwidth,        // bandwidth in kHz (7.8–500)
+        spreading_factor, // spreading factor (6–12)
+        coding_rate,      // coding rate denominator (5–8)
+        sync_word,        // sync word (0x34 public, 0x12 private)
+        power,            // power in dBm
+        preamble_length,  // preamble length
+        gain,             // gain (0 = auto)
+        TCXO              // TCXO enabled
     );
+
     if (state != RADIOLIB_ERR_NONE)
     {
         Serial.printf("failed (%d)\n", state);
     }
+
     // Heltec V3 routes DIO2 to the RF switch — enable it
     radio.setDio2AsRfSwitch(true);
 
--- main/src/mesh.h
+++ main/src/mesh.h
@@ -10,13 +10,24 @@ class meshtastic
 {
 private:
     SX1262 radio;
+    
     bool initialized;
     int id;
+
+    const float frequency = 868.0;
+    const float bandwidth = 125.0;
+    const int spreading_factor = 7;
+    const int coding_rate = 7;
+    const int sync_word = 0x34;
+    const float power = 20;
+    const int preamble_length = 8;
+    const int gain = 0;
+    const bool TCXO = false;
 
 public:
     meshtastic();
     ~meshtastic();
-    
+
     start_radio();
     sleep_radio();
     init_mesh();
