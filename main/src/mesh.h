8fa17e6040b504b4d109c83050eafccf428ced1d
marcus <marcus.mr2003@gmail.com>
2025-11-10 09:30:25 +0100

    Revert "mesh add"
    This reverts commit e8dc1b219f355ef762901f2fcb21e530123f0280.

--- main/src/mesh.h
+++ main/src/mesh.h
@@ -2,24 +2,6 @@
 #define MESH_H
 
 #include <Arduino.h>
-#include <RadioLib.h>
-#include <SPI.h>
-#include <vector>
-
-class meshtastic
-{
-private:
-    SX1262 radio;
-    bool initialized;
-    int id;
-
-public:
-    meshtastic();
-    ~meshtastic();
-    
-    start_radio();
-    sleep_radio();
-    init_mesh();
-};
+#include "../include/RadioLib-master/src/RadioLib.h"
 
 #endif
