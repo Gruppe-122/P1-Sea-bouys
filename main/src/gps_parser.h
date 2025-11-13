4d66a513ee8c6d0d28f22f47d67f75fbba21c05a
beep-boop-o7 <andreas@lorenzen.us>
2025-11-13 12:28:17 +0100

    gps parser

---
+++ main/src/gps_parser.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <HardwareSerial.h>
+#include <string.h>
+
+typedef struct nmeaData;
+
+nmeaData nmeaTestData;
+
+void readGNSS(nmeaData *data, HardwareSerial &serPort)
+
+void initGNSS(HardwareSerial &serPort)
