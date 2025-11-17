74f425535553f1b56f0f68f52b5b0f8c9fc93594
beep-boop-o7 <andreas@lorenzen.us>
2025-11-17 14:52:50 +0100

    Update gps_parser.h

--- main/src/gps_parser.h
+++ main/src/gps_parser.h
@@ -9,17 +9,17 @@ typedef struct nmeaData;
 * @param sleepTime how long in seconds the gps will sleep.
 * @param serPort serial port gps module is conected to.
 */
-void sleepGNSS(int sleepTime, HardwareSerial &serPort)
+void sleepGNSS(int sleepTime, HardwareSerial &serPort);
 /**
 * @brief reads GNSS data from gps module.
 * @param data a struct of type "nmeaData", passed as &varName, data will be stored in the struct.
 * @param serPort serial port gps module is conected to.
 */
-void readGNSS(nmeaData *data, HardwareSerial &serPort)
+void readGNSS(nmeaData *data, HardwareSerial &serPort);
 /**
 * @brief initilises gps module on passed serial port.
 * @param serPort serial port gps module is conected to.
 * @param RX_pin rx pin gps module is conected to.
 * @param TX_pin tx pin gps module is conected to.
 */
-void initGNSS(HardwareSerial &serPort, int RX_pin, int TX_pin)
+void initGNSS(HardwareSerial &serPort, int RX_pin, int TX_pin);
