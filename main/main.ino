0f59f1f57fb338a66fe118f639555c1938eabedb
Cwoasount <78689800+eKondrup@users.noreply.github.com>
2025-11-17 11:45:27 +0100

    Feature/voltagedivider (#5)
    * Add Voltagedivider, h and src file
    
    * changes in Volt src and h file. changes in main.ino add correct libraries

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,45 @@
+// -----------------------------------------------------------------------
+// | Main sketch Bouy project                                            |
+// | Board: Heltec WiFi LoRa 32(V3)                                      |
+// | Pin layout:                                                         |
+// |  ACS712  -> pin x                                                   |
+// |                                                                     |
+// | Make sure ESP32 is properly inserted in breadboard or DC_OFFSET     |
+// | will increase                                                       |
+// -----------------------------------------------------------------------
+
 #include "src/accel.h"
-#include "src/amp.h"
+#include "src/current.h"
 #include "src/gps.h"
-#include "esp_sleep.h"
+#include "src/volt.h"
+
+//definitions
+#define R1 1000.0        //Resistor value in voltagedivider circuit
+#define R2 1000.0        //Resistor value in voltagedivider circuit
+#define REF_VOLTAGE 1100 //ESP32 reference voltage for calibration.
+#define VOLT_PIN 7
+#define ADC_RESOLUTION 12
+
+#define CURRENTSENSOR_PIN 6
+#define DC_OFFSET 2500   //voltage offset from currentsensor module
+
+#define ADC_N_SAMPLES 20          //amount of ADC signals to base voltage reading on
+#define ADC_SAMPLING_FREQUENCY 20 //time between taking ADC value ms
+//Variables
+
+//Objects
+Volt battery(VOLT_PIN, REF_VOLTAGE, R1, R2, ADC_ATTEN_DB_11, ADC_RESOLUTION);
+CurrentSensor current(CURRENTSENSOR_PIN, DC_OFFSET);
 
 void setup()
 {
   delay(1000);
   Serial.begin(115200);
-  accelSetup();
-  calibrate();
-
-  if (accelerometer() == 1)
-  {
-    Serial.println("sending msg to port");
-    delay(10000);
-  }
-
-  esp_sleep_enable_ext0_wakeup((gpio_num_t)5, 1);
-  esp_deep_sleep_start();
+  battery.set_sampling(ADC_N_SAMPLES, ADC_SAMPLING_FREQUENCY);
+  current.set_sampling(ADC_N_SAMPLES, ADC_SAMPLING_FREQUENCY);
+  current.begin();
 }
-void loop() {
 
-};
+void loop() {
+  
+}
