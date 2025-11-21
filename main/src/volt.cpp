422c692110a6e5a20f10d6aa9de303732ec3a4ed
Emil <emilakkristensen@gmail.com>
2025-11-21 14:26:06 +0100

    update from main code

--- main/src/volt.cpp
+++ main/src/volt.cpp
@@ -1,21 +1,22 @@
 #include "volt.h"
 
-Volt::Volt(int pin, int vRef, float R1, float R2,
-           adc_atten_t atten, int resolution)
+Volt::Volt(int pin, float R1, float R2,
+           adc_attenuation_t atten, int resolution)
     : _pin(pin),
-      _vRef(vRef),
       _resolution(resolution),
       _atten(atten),
-      _samples(8),
-      _tid_m_samples(5),
+      _samples(20),
+      _tid_m_samples(20),
       _R1(R1),
       _R2(R2)
 {
-    _divFactor = R1 / (R1 + R2);
+    _divFactor = _R2 / (_R1 + _R2);
+}
 
+void Volt::begin()
+{
     analogReadResolution(_resolution);
-    analogSetPinAttenuation(_pin, (adc_attenuation_t)_atten);
-    esp_adc_cal_characterize(ADC_UNIT_1, (adc_atten_t)_atten, _resolution_ADC, _vRef, &_adcChars);
+    analogSetPinAttenuation(_pin, _atten);
 }
 
 void Volt::set_sampling(int samples, int tid_m_samples)
@@ -27,22 +28,22 @@ void Volt::set_sampling(int samples, int tid_m_samples)
 int Volt::avg_ADC()
 {
     long sum = 0;
-    for (int i = 0; i < _samples; i++)
-    {
+
+    for (int i = 0; i < _samples; i++) {
         sum += analogRead(_pin);
         delay(_tid_m_samples);
     }
-    return (float)sum / _samples;
+
+    return sum / _samples;
 }
 
-float Volt::read_ADC_voltage()
+float Volt::read_voltage_mV()
 {
-    int rawAvg = (int)avg_ADC();
-    return esp_adc_cal_raw_to_voltage(rawAvg, &_adcChars);
+    return analogReadMilliVolts(_pin);
 }
 
 float Volt::read_battery_voltage_mV()
 {
     float mv = analogReadMilliVolts(_pin);
-    return (mv / _divFactor);
+    return mv / _divFactor;
 }
--- main/src/volt.h
+++ main/src/volt.h
@@ -2,18 +2,17 @@
 #define VOLT_H
 
 #include <Arduino.h>
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
 
 /**
  * @class Volt
- * @brief Reads voltage from an ADC pin
+ * Implements functionality to read voltage from ADC pin
+ * and calculating source voltage from voltagedivider circuit
  *
  * Example usage:
  * @code
- * Volt battery(7, 1100, 10000.0, 10000.0, ADC_ATTEN_DB_11, 12);
- * battery.setSampling(20, 20);
- * battery.readBatteryVoltage();
+ * Volt battery(7, 1100, 10000.0, 10000.0, ADC_11db, 12);
+ * battery.set_sampling(20, 20);
+ * battery.read_battery_voltage_mV();
  * @endcode
  */
 class Volt
@@ -22,11 +21,24 @@ public:
     /**
      * @brief Constructor for the Volt class
      * @param pin GPIOpin connected to voltage source, make sure pin has an ADC_Channel.
+     * @param R1  Resistor value in voltagedivider
+     * @param R2  The resistor value the voltage is measured across
      */
-    Volt(int pin, int vRef, float R1, float R2,
-         adc_atten_t atten = ADC_ATTEN_DB_11,
+    Volt(int pin, float R1, float R2,
+         adc_attenuation_t atten = ADC_11db,
          int resolution = 12);
 
+    /**
+     * @brief begin method for Volt class
+     * Configures the ESP32 to recieve voltage on _pin.
+     * Executes:
+     * @code
+     * analogReadResolution(_resolution);
+     * analogSetPinAttenuation(_pin, (adc_attenuation_t)_atten);
+     * @endcode
+     * @note Must be called before taking any readings.
+     */
+    void begin();
     /**
      * @brief sets the sampling for avg_ADC()
      * @param samples Amount of samples it calculates the average out of
@@ -41,20 +53,19 @@ public:
      * @code
      * analogReadResolution(ADC_RESOLUTION);
      * analogSetAttenuation(ADC_11db);
-     * esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, &adc_chars);
      * @endcode
      * @return average ADC value
      */
     int avg_ADC();
     /**
-     * @brief read_ADC_voltage method for Volt class
-     * gets the voltage based on the average ADC signal from esp_adc_cal_raw_to_voltage
+     * @brief read_battery_voltage_mv method for Volt class
+     * gets the voltage from API analogReadMilliVolts()
      * @return voltage in mV
      */
-    float read_ADC_voltage();
+    float read_voltage_mV();
     /**
      * @brief read_battery_voltage_mv method for Volt class
-     * gets the voltage from analogReadMilliVolts()
+     * gets the voltage from API analogReadMilliVolts()
      * and divides with divFactor based on R1 and R2
      * Such that you get the voltage from the bouy battery
      * @return voltage in mV
@@ -63,17 +74,15 @@ public:
 
 private:
     int _pin;
-    int _vRef;
-    adc_bits_width_t _resolution_ADC = ADC_WIDTH_BIT_12;
     int _resolution;
-    adc_atten_t _atten;
-
-    int _samples = 20;
-    int _tid_m_samples = 20;
+    // Arduino API uses adc_attenuation_t
+    // Where ESP_IDF uses adc_atten_t these cannot be mixed
+    adc_attenuation_t _atten = ADC_11db;
+    int _samples;
+    int _tid_m_samples;
     float _R1;
     float _R2;
     float _divFactor;
-    esp_adc_cal_characteristics_t _adcChars;
 };
 
 #endif
