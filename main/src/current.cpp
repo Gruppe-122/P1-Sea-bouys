a2875ec35988188d8ec3797451035ab0873a8e43
Emil <emilakkristensen@gmail.com>
2025-12-04 15:47:59 +0100

    fix ovf issue. add moving_avg_ADC(), add type-cast conversion of atten, add resolution argument to contructor

--- main/src/current.cpp
+++ main/src/current.cpp
@@ -1,17 +1,18 @@
 #include "current.h"
 
-CurrentSensor::CurrentSensor(int pin, int dcOffset_mV, int modSensitivity_mV_per_A, adc_attenuation_t atten)
+CurrentSensor::CurrentSensor(int pin, int dcOffset_mV, int modSensitivity_mV_per_A, adc_attenuation_t atten, uint8_t resolution)
 {
   _pin = pin;
   _dcOffset_mV = dcOffset_mV;
   _mod_mV_per_A = modSensitivity_mV_per_A;
-  _atten = atten;
+  _atten = (adc_attenuation_t)atten;
+  _adc_resolution = (uint8_t)resolution;
 }
 
 void CurrentSensor::begin()
 {
-  analogReadResolution(_adc_resolution);          // Set ADC resolution
-  analogSetPinAttenuation(_pin, _atten);        // Set input attenuation for 0-3.1V range
+  analogReadResolution(_adc_resolution); // Set ADC resolution
+  analogSetPinAttenuation(_pin, _atten); // Set input attenuation for 0-3.1V range
 }
 
 void CurrentSensor::set_sampling(int samples, int tid_m_samples, int adc_resolution)
@@ -19,6 +20,32 @@ void CurrentSensor::set_sampling(int samples, int tid_m_samples, int adc_resolut
   _samples = samples;
   _tid_m_samples = tid_m_samples;
   _adc_resolution = adc_resolution;
+}
+
+// Direct import from volt.cpp
+uint CurrentSensor::moving_avg_ADC()
+{
+    uint new_sample = analogRead(_pin);
+
+    // Remove oldest sample from sum
+    ma_sum -= ma_buffer[ma_index];
+
+    // Insert newest sample
+    ma_buffer[ma_index] = new_sample;
+    ma_sum += new_sample;
+
+    // Next index
+    ma_index++;
+    if (ma_index >= _ma_samples)
+    {
+        ma_index = 0;
+        ma_full = true;
+    }
+
+    if (!ma_full)
+        return ma_sum / ma_index;
+
+    return ma_sum / _ma_samples;
 }
 
 int CurrentSensor::avg_ADC(int samples, int tid_m_samples)
@@ -33,7 +60,8 @@ int CurrentSensor::avg_ADC(int samples, int tid_m_samples)
   }
   return sum / samples;
 }
-
+// Potential improvement: Use moving average filter instead of simple average
+// and lookup table for better accuracy
 float CurrentSensor::get_voltage_mV()
 {
   return analogReadMilliVolts(_pin);
@@ -41,8 +69,9 @@ float CurrentSensor::get_voltage_mV()
 
 float CurrentSensor::measure_current_A()
 {
-  uint32_t voltage_mV = get_voltage_mV();
-  float amps = (voltage_mV - _dcOffset_mV) / _mod_mV_per_A;
+  float voltage_mV = get_voltage_mV();
+  // typecast to float to avoid integer division issues
+  float amps = (voltage_mV - (float)_dcOffset_mV) / _mod_mV_per_A;
   return amps;
 }
 
